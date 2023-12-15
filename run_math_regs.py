bc70c6e45bf5570163f990a0444a23fa77cc7ac0
Clara Cullum <claracullum@gmail.com>
2023-12-15 00:34:34 -0500

    rs

--- run_math_regs.py
+++ run_math_regs.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
+import matplotlib.pyplot as pyplot
 
 data1 = pd.read_csv("results.csv")
 dataset_final = pd.read_csv("dataset_final.csv")
@@ -8,28 +9,43 @@ dataset_final = pd.read_csv("dataset_final.csv")
 math_data = pd.concat([data1, dataset_final['math']], axis=1)
 math_data = math_data[:21642]
 
+#to show relationship between each factor and math ability
+pyplot.scatter(math_data.iloc[:, 0], math_data.iloc[:, 4])
+pyplot.savefig("math_on_factorX.png")
+pyplot.close()
+
+pyplot.scatter(math_data.iloc[:, 1], math_data.iloc[:, 4])
+pyplot.savefig("math_on_factorY.png")
+pyplot.close()
+
+pyplot.scatter(math_data.iloc[:, 2], math_data.iloc[:, 4])
+pyplot.savefig("math_on_factorZ.png")
+pyplot.close()
+
+pyplot.scatter(math_data.iloc[:, 3], math_data.iloc[:, 4])
+pyplot.savefig("math_on_factorW.png")
+pyplot.close()
+
+
 target = math_data['math']
 data_factors = math_data[['Factor X', 'Factor Y', 'Factor Z', 'Factor W']]
 
-# Create linear regression models
 machineX = LinearRegression()
 machineY = LinearRegression()
 machineZ = LinearRegression()
 machineW = LinearRegression()
 
-# Fit the models
 machineX.fit(data_factors[['Factor X']], target)
 machineY.fit(data_factors[['Factor Y']], target)
 machineZ.fit(data_factors[['Factor Z']], target)
 machineW.fit(data_factors[['Factor W']], target)
 
-# Make predictions
 predictionsX = machineX.predict(data_factors[['Factor X']])
 predictionsY = machineY.predict(data_factors[['Factor Y']])
 predictionsZ = machineZ.predict(data_factors[['Factor Z']])
 predictionsW = machineW.predict(data_factors[['Factor W']])
 
-# Calculate R squared scores
+#all r squared 
 r2_x = r2_score(target, predictionsX)
 print("Factor X R square score: ", r2_x)
 print("\n\n")
@@ -45,3 +61,5 @@ print("\n\n")
 r2_w = r2_score(target, predictionsW)
 print("Factor W R square score: ", r2_w)
 print("\n\n")
+
+
